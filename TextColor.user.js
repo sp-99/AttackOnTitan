56b4ccc6633ddcf8ad413b0b5d2f5cacd8b28eb4
Annie Leonhardt <damanitheone@gmail.com>
2014-11-01 18:02:10 -0700

    Create TextColor.user.js
    Changes the color of your text automatically on the Oneplus forums.

---
+++ TextColor.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Text Colorize
+// @namespace    https://github.com/annieleonhardt/AttackOnTitan
+// @version      V1.03
+// @description  changes the color of your text automatically
+// @author       Annie Leonhardt
+// @match        *.oneplus.net*
+// @grant        none
+// ==/UserScript==
+
+//colorizes the text for your replies
+function colorize() {
+  if (document.getElementsByClassName('redactor_textCtrl redactor_MessageEditor redactor_BbCodeWysiwygEditor redactor_NoAutoComplete') [0]) {
+    $('iframe.redactor_textCtrl.redactor_MessageEditor.redactor_BbCodeWysiwygEditor.redactor_NoAutoComplete').contents().find('p').css({'color': '#4183c4', 'font-size': 'small'})
+  } else if (document.getElementsByClassName('redactor_textCtrl redactor_MessageEditor redactor_BbCodeWysiwygEditor redactor_') [0]) {
+    $('iframe.redactor_textCtrl.redactor_MessageEditor.redactor_BbCodeWysiwygEditor.redactor_').contents().find('p').css({'color': '#4183c4', 'font-size': 'small'})
+  }
+}
+
+window.addEventListener('submit', colorize, true);
+HTMLFormElement.prototype._submit = HTMLFormElement.prototype.submit;
+HTMLFormElement.prototype.submit = colorize;
